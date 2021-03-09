5b9889efd19a704b50900f44e129f65c95baad5c
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-03-09 19:28:22 +0100

    feat: mobile Banner fixed heigh

--- src/components/App/Header/StatelessHeader.tsx
+++ src/components/App/Header/StatelessHeader.tsx
@@ -26,7 +26,7 @@ const StatelessHeader: React.FC<IStatelessHeaderProps & HeaderData> = ({
         ` flex transition-all duration-300 ease-in-out items-center  top-0 left-0 z-50 w-full  `,
         isTransparent ? `bg-transparent ` : `bg-white bg-opacity-95 shadow `,
         isFixed ? `fixed` : `sticky`,
-        isBig ? `h-36` : `h-20`,
+        isBig ? `md:h-36 h-20` : `h-20`,
       )}
     >
       <div className="flex items-center w-full h-full c-container">
