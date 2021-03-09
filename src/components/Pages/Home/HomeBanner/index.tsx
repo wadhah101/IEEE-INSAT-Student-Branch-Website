5f3b92d511e868e5262ab49516f16b167b188ce6
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-03-09 20:41:09 +0100

    feat: layout FIx

--- src/components/Pages/Home/HomeBanner/index.tsx
+++ src/components/Pages/Home/HomeBanner/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import clsx from 'clsx';
 import HomeBannerButtonBlock from './HomeBannerButtonBlock';
 import HomeBannerWriting from './HomeBannerWriting';
-import * as style from './HomeBanner.module.css';
 import HomeBannerMediaIcons from './HomeBannerMediaIcons';
 
 interface IHomeBannerProps {}
@@ -24,22 +23,15 @@ const HomeBanner: React.FC<IHomeBannerProps> = () => {
       <div className="absolute top-0 left-0 z-10 w-full h-full bg-black bg-opacity-60 " />
       {/* content */}
       <div className="relative z-20 flex flex-col min-h-screen text-white ">
-        <div
-          className={clsx(
-            `grid h-full flex-grow w-full c-container`,
-            style.centerVertical,
-          )}
-        >
+        <div className="flex flex-col flex-grow w-full h-full c-container">
           <div />
-          <div className="w-full ">
+          <div className="flex flex-col justify-center flex-grow w-full md:items-start ">
             {/* title block */}
             <HomeBannerWriting />
 
             {/* button block */}
-            <div className="mt-6 md:mt-8 ">
-              <div className="flex-grow mb-2 ">
-                <HomeBannerButtonBlock />
-              </div>
+            <div className="mt-3 md:mt-8 ">
+              <HomeBannerButtonBlock />
             </div>
           </div>
           <div className="flex flex-col-reverse pb-4 md:pb-6">
