0550cac88b5d83292cf39b1dbbfa351bd4cf64ae
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-03-16 22:21:18 +0100

    feat: color changing demo

--- src/components/Pages/Home/ChapterElement/ChapterElement.tsx
+++ src/components/Pages/Home/ChapterElement/ChapterElement.tsx
@@ -1,17 +1,52 @@
+/* eslint-disable implicit-arrow-linebreak */
+import clsx from 'clsx';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface IChapterElementProps {
   data: GatsbyTypes.ContentfulChapter;
 }
 
 const ChapterElement: React.FC<IChapterElementProps> = ({ data }) => {
+  const [isHoveringMulti, setIsHoveringMulti] = useState({
+    out: false,
+    in: false,
+  });
+  const isHovering = isHoveringMulti.in || isHoveringMulti.out;
+
   return (
-    <div className="flex p-6 ">
-      <GatsbyImage
-        image={getImage(data.logos[1].localFile as any)}
-        alt={data.title}
-      />
+    <div
+      className={clsx(`flex h-full  items-center  p-6 cursor-pointer`)}
+      onMouseEnter={() => setIsHoveringMulti((e) => ({ ...e, out: true }))}
+      onMouseLeave={() => setIsHoveringMulti((e) => ({ ...e, out: false }))}
+    >
+      <div className="relative ">
+        <GatsbyImage
+          className={clsx(
+            isHovering ? `opacity-0` : `opacity-100`,
+            `duration-300 ease-in-out transition-opacity`,
+          )}
+          image={getImage(data.logos[1].localFile as any)}
+          alt={`${data.title} logo monochrome`}
+        />
+
+        <div
+          className={clsx(
+            `absolute duration-300 ease-in-out transition-opacity top-0 left-0 z-10 w-full h-full `,
+            [isHovering ? `opacity-100` : `opacity-0`],
+          )}
+        >
+          <GatsbyImage
+            onMouseEnter={() => setIsHoveringMulti((e) => ({ ...e, in: true }))}
+            onMouseLeave={
+              () => setIsHoveringMulti((e) => ({ ...e, in: false }))
+              // eslint-disable-next-line react/jsx-curly-newline
+            }
+            image={getImage(data.logos[0].localFile as any)}
+            alt={`${data.title} logo color`}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Pages/Home/ChapterGrid/index.tsx
+++ src/components/Pages/Home/ChapterGrid/index.tsx
@@ -6,7 +6,7 @@ interface IindexProps {
   chapters: GatsbyTypes.ContentfulChapter[];
 }
 
-const chaptersOrder = [`PES`, `RAS`, `IAS`, `NPSS`, `EMBS`, `CS`];
+const chaptersOrder = [`RAS`, `CS`, `IAS`, `PES`, `EMBS`, `NPSS`];
 
 const ChapterGrid: React.FC<IindexProps> = ({ chapters }) => {
   const data = chaptersOrder.map((e) =>
@@ -14,13 +14,14 @@ const ChapterGrid: React.FC<IindexProps> = ({ chapters }) => {
     chapters.find((e1) => e1.acronym === e),
   );
   return (
-    <div className="py-20 bg-sb-blue-main-500">
+    <div className="py-16 bg-sb-blue-main-500">
       {/* <h2 className="mb-12 text-5xl font-bold tracking-wider text-center text-white ">
         OUR CHAPTERS
       </h2> */}
+
       <div
         style={{ gridAutoRows: `1fr` }}
-        className="grid items-center grid-cols-3 gap-x-20 gap-y-16 justify-items-center c-container "
+        className="grid items-center grid-cols-3 gap-x-16 gap-y-16 justify-items-center c-container "
       >
         {data.map((e) => (
           // eslint-disable-next-line react/no-array-index-key
