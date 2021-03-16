633a52a5386660f40cbca4c9980a0f5849e00149
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-03-17 00:32:00 +0100

    fix: added gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-
+    'gatsby-plugin-gatsby-cloud',
     // cms
     {
       resolve: 'gatsby-source-contentful',
